770683c8a0d9f8211a8acce32b61b63769006bc4
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-06 23:56:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_t
 # @st.cache_resource()
 def download_file_from_drive(url, output):
     gdown.download(url, output, quiet=False)
-# To clear the resource 
-download_file_from_drive.clear()
 
 # opening the image
 # image = open('banner_image.jpeg', 'rb').read()
@@ -30,35 +28,45 @@ st.markdown("Predict croissant quality")
 st.markdown("---")
 
 
-
 # Load the trained model
 #model = load_model('model.h5')
 # Google Drive file link (shared link)
 file_url = 'https://drive.google.com/uc?1_wYk2Zo_YkINqoRwSf3L_DUjkjHFwV08'
 output_file = 'model.h5'
 
-download_file_from_drive(file_url, output_file)
-st.success('Model downloaded successfully!')
+if "model" not in st.session_state.keys():
 
+    download_file_from_drive(file_url, output_file)
+    st.success('Model downloaded successfully!')
 
-# Check if the file exists
-if os.path.exists(output_file):
-    # Check if the file is indeed an .h5 file by size (usually > few MBs)
-    file_size = os.path.getsize(output_file)
-    #st.write(f"Downloaded file size: {file_size / (1024 * 1024)} MB")
-    
-    if file_size > 0:  # Ensuring the file is not empty
-        try:
-            with st.spinner('Loading model...'):
-                model = load_model(output_file)
-                st.success('Model loaded successfully!')
-                #st.write(model.summary())
-        except OSError as e:
-            st.error(f"Error loading model: {e}")
+    # Check if the file exists
+    if os.path.exists(output_file):
+        # Check if the file is indeed an .h5 file by size (usually > few MBs)
+        file_size = os.path.getsize(output_file)
+        #st.write(f"Downloaded file size: {file_size / (1024 * 1024)} MB")
+        
+        if file_size > 0:  # Ensuring the file is not empty
+            try:
+                with st.spinner('Loading model...'):
+                    st.session_state["model"] = load_model(output_file)
+                    st.success('Model loaded successfully!')
+                    st.write(model.summary())
+            except OSError as e:
+                st.error(f"Error loading model: {e}")
+        else:
+            st.error('Downloaded file is empty or corrupted.')
     else:
-        st.error('Downloaded file is empty or corrupted.')
-else:
-    st.error('Error: Model file not found.')
+        st.error('Error: Model file not found.')
+
+    
+model = st.session_state["model"]
+
+# Uncomment to reload the model
+# del st.session_state["model"]
+
+
+
+
 
 
 
