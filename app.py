111e3b19226564b5fff7c7c93cae074a89ac7d0a
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-06 23:16:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 import pickle
 import tempfile
 from PIL import ImageFont, ImageDraw, Image
-from efficientnet.tfkeras import EfficientNetB4
+#from efficientnet.tfkeras import EfficientNetB4
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
 
