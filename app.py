968492d0a15afb826d5db37900017273844f4d0f
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-06 23:37:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,29 @@ st.markdown("---")
 file_url = 'https://drive.google.com/uc?1_egnP-PtljAfWAtzwVbq3d8YbFLZPDPy'
 output_file = 'model.h5'
 
+download_file_from_drive(file_url, output_file)
+st.success('Model downloaded successfully!')
+
+
+# Check if the file exists
+if os.path.exists(output_file):
+    # Check if the file is indeed an .h5 file by size (usually > few MBs)
+    file_size = os.path.getsize(output_file)
+    #st.write(f"Downloaded file size: {file_size / (1024 * 1024)} MB")
+    
+    if file_size > 0:  # Ensuring the file is not empty
+        try:
+            with st.spinner('Loading model...'):
+                model = load_model(output_file)
+                st.success('Model loaded successfully!')
+                #st.write(model.summary())
+        except OSError as e:
+            st.error(f"Error loading model: {e}")
+    else:
+        st.error('Downloaded file is empty or corrupted.')
+else:
+    st.error('Error: Model file not found.')
+
 
 
 st.header("Upload your image")
